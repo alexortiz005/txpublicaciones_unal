9e00f838110064e13f816fba91beab86d9c1a5c9
alexortiz005 <eaortiz@unal.edu.co>
2018-06-20 13:22:24 -0500

    se completa la implementacion de preview, falta implementar las vistas responsive

--- Classes/Controller/PublicacionController.php
+++ Classes/Controller/PublicacionController.php
@@ -34,9 +34,10 @@ class PublicacionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
     {
 
         $uri_background = 'fileadmin/user_upload/' . $this->settings['background'];
+        $uri_imagen_link = 'fileadmin/user_upload/' . $this->settings['imagen_link'];
         $uri_link = $this->settings['link'];
-        $titulo = $this->settings['titulo'];
-        $subtitulo = $this->settings['subtitulo'];
+        $titulo_link = $this->settings['titulo_link'];
+        $texto_link = $this->settings['texto_link'];
         $uids_publicaciones_a_mostrar = explode(',', $this->settings['publicaciones_a_mostrar']);
 
         $publicaciones_a_mostrar = [];
@@ -47,9 +48,10 @@ class PublicacionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
 
         $this->view->assignMultiple([
             'uri_background' => $uri_background, 
+            'uri_imagen_link' => $uri_imagen_link, 
             'uri_link' => $uri_link, 
-            'titulo' => $titulo, 
-            'subtitulo' => $subtitulo, 
+            'titulo_link' => $titulo_link, 
+            'texto_link' => $texto_link, 
             'publicaciones' => $publicaciones_a_mostrar
         ]);
 
--- Resources/Public/Js/preview.js
+++ Resources/Public/Js/preview.js
@@ -1,9 +1,29 @@
+String.prototype.trunc = String.prototype.trunc ||
+      function(n){
+          return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
+      };
+
 $( document ).ready(function() {
 	var uriBackground = $('#uri_background').val()
 	$('.frame-jumbotron').each(function() {
 		if($(this).find('.preview-publicaciones').length>0){
 			$(this).css('background-image', 'url(' + uriBackground + ')');
 			$(this).css( 'background-size', 'cover');
+		}
+	});
+
+	$('.croptext').find('p').each(function() {
+		var text=$(this).html()
+		$(this).html(text.trunc(135))
+		console.log()
+	});
+
+	$(".preview-publicacion").click(function()
+	{
+		if($(this).data('link')=="NULL"){
+			alert('Unavailable');
+		}else{
+	    	document.location.href = $(this).data('link');
 		}
 	});
 });
